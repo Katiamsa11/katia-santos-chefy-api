327ba4470b7f06201710595bed14c802c0a4236d
katia santos <katiamsa@gmail.com>
2022-09-18 23:21:21 -0400

    created and finalize read me file

---
+++ README.md
@@ -0,0 +1,50 @@
+# ABOUT CHEFY
+
+Chefy is a web-based platform to connect chefs with households for private in-home dining experiences. Clients can search for and book the chef best suited for their event. Tools used: React, Sass, CSS-grid, Node, Express and MySQL for the database.
+Thank you for checking my project!
+
+## SET UP BACK-END
+
+### Follow these steps to run a local instance of Chefy:
+
+You will need node, npm , and MYSQL already installed in your computer.
+
+1. Clone or download this repository.
+2.Create a database using MYSQL and call it the following:
+
+```
+chefyData
+```
+
+3. Install server dependencies running the following on your terminal:
+
+```
+npm install
+```
+
+3. Run npm start to load the react app
+
+```
+npm start
+```
+
+4. Update ./knex.js file with your connection details and database to match.
+```
+PORT = <PORT_NUMBER>
+DB_HOST = <HOST_ADDRESS>
+DB_USER = <YOUR DB USERNAME>
+DB_PASSWORD = <YOUR DB PASSWORD>
+DB_DATABASE = chefyData
+```
+5.Run migrations file.
+```
+npm run migrate or npx knex migrate:latest
+```
+6. Run seed file to populate data on the database.
+```
+npm run seed or npx knex seed:run
+```
+7. Start the server.
+```
+npm run dev or node server.js
+```
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const twilio = require('twilio');
+const twilio = require("twilio");
 const app = express();
 const chefsRoutes = require("./routes/chefsRoute.js");
 const PORT = process.env.PORT || 8080;
@@ -16,21 +16,22 @@ const authToken = "8af1ebdbb763f3852b50207372b9a697";
 const client = new twilio(accountSid, authToken);
 
 //send Text
-app.get('/send-text', (req, res) => {
+app.get("/send-text", (req, res) => {
   //Welcome Message
-  res.send('Hello to the Twilio Server')
+  res.send("Hello to the Twilio Server");
 
   //_GET Variables
   const { recipient, textmessage } = req.query;
 
-
   //Send Text
-  client.messages.create({
+  client.messages
+    .create({
       body: textmessage,
-      to: recipient,  // Text this number
-      from: '+17079857362' // From a valid Twilio number
-  }).then((message) => console.log(message.body));
-})
+      to: recipient, // Text this number
+      from: "+17079857362", // From a valid Twilio number
+    })
+    .then((message) => console.log(message.body));
+});
 
 // added middleware to allow to serve static files
 app.use("/assets", express.static("./assets"));
