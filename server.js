a0fd69d0f09c72657d4cdb26d360e1c75d66daac
katia santos <katiamsa@gmail.com>
2022-09-28 19:44:12 -0400

    fixing cors error on deployment heroku

--- server.js
+++ server.js
@@ -9,7 +9,13 @@ const reviewsRoutes = require("./routes/reviewsRoute.js");
 const eventsRoutes = require("./routes/eventsRoute.js");
 const PORT = process.env.PORT || 8080;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://chefsdelivered.netlify.app"],
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
